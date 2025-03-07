758e0bf5f4880b18f99a2dadfc71a352bb4b1201
OlawoyeTaofeek <habephe21@gmail.com>
2025-03-07 18:39:45 +0100

    Users model created successfully

--- advancedAPI/models.py
+++ advancedAPI/models.py
@@ -19,10 +19,12 @@ class Post(Base):
     # created_at = Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('now()'))
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "Users"
     
-    id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String, nullable=False, db_index=True, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False, index=True)
+    name = Column(String(255), nullable=False)
+    username = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
